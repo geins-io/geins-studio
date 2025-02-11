1f8b2f4470985ce1b0f870ec16d0053df88fc495
Olivia Axelsson <axelsson@geins.io>
2025-02-11 13:52:42 +0100

    fix: reset start page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,4 @@
-<script setup lang="ts">
-const showData = ref();
-const clickHandler = async () => {
-  const apiEndpoint = '/account/language/list';
-  const { data, error } = await useAPI<Entity[]>(apiEndpoint);
-  showData.value = data.value;
-};
-</script>
+<script setup lang="ts"></script>
 <template>
-  <div>
-    <Button @click="clickHandler">Click me</Button>
-    <pre>
-      {{ showData }}
-    </pre>
-  </div>
+  <div />
 </template>
