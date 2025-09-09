0ebb66cca490ea5e2b1ff6d8cdc18269f463da13
Olivia Axelsson <axelsson@geins.io>
2025-09-09 09:51:11 +0200

    chore: add insights to auth layout

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+
 import Logo from '@/assets/logos/geins.svg';
 </script>
 
@@ -6,13 +8,14 @@ import Logo from '@/assets/logos/geins.svg';
   <div
     class="flex h-screen w-full flex-col-reverse justify-end lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[600px]"
   >
-    <div class="flex items-center justify-center bg-card py-12">
+    <div class="bg-card flex items-center justify-center py-12">
       <div class="grid w-full gap-6 px-10 lg:w-[350px] lg:p-0">
         <slot />
       </div>
     </div>
-    <div class="flex items-center justify-center bg-background">
+    <div class="bg-background flex items-center justify-center">
       <Logo class="size-32 lg:size-1/3" :font-controlled="false" />
     </div>
   </div>
+  <SpeedInsights />
 </template>
