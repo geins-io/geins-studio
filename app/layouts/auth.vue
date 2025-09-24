f87799031fd44034bb4c301222ac22382327ff29
Olivia Axelsson <axelsson@geins.io>
2025-09-24 15:50:48 +0200

    feat: responsive login

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -6,15 +6,20 @@ import Logo from '@/assets/logos/geins.svg';
 
 <template>
   <div
-    class="flex h-screen w-full flex-col-reverse justify-end lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[600px]"
+    class="flex h-screen w-full flex-col-reverse justify-center max-lg:items-center max-sm:p-4 lg:grid lg:min-h-[600px] lg:grid-cols-2"
   >
-    <div class="bg-card flex items-center justify-center py-12">
+    <div
+      class="bg-card flex w-full items-center justify-center py-12 max-lg:max-w-[470px] max-lg:rounded-lg max-lg:border"
+    >
       <div class="grid w-full gap-6 px-10 lg:w-[350px] lg:p-0">
         <slot />
       </div>
     </div>
     <div class="bg-background flex items-center justify-center">
-      <Logo class="size-32 lg:size-1/3" :font-controlled="false" />
+      <Logo
+        class="-mt-7 mb-7 w-28 sm:w-32 lg:m-0 lg:w-1/3"
+        :font-controlled="false"
+      />
     </div>
   </div>
   <SpeedInsights />
