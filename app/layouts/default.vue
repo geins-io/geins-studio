230b7d0ef4b77e0786aad1ee7d4e7f6642c66ef7
Olivia Axelsson <axelsson@geins.io>
2025-09-25 11:23:08 +0200

    feat: viewport module WIP

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -8,7 +8,25 @@ import {
   SIDEBAR_COOKIE_MAX_AGE,
 } from '../components/ui/sidebar/utils';
 
+const viewport = useViewport();
+
+// Debug: watch breakpoint changes
+watch(viewport.breakpoint, (newBreakpoint, oldBreakpoint) => {
+  console.log('Breakpoint changed:', oldBreakpoint, '->', newBreakpoint);
+  console.log('isLessThan md:', viewport.isLessThan('md'));
+});
+
 const currentSidebarWidth = computed(() => {
+  // Access .value to establish reactive dependency
+  const currentBreakpoint = viewport.breakpoint.value;
+  const isLessThanMd = viewport.isLessThan('md');
+
+  console.log('Computing sidebar width:', { currentBreakpoint, isLessThanMd });
+
+  if (isLessThanMd) {
+    console.log('returning 0 for mobile');
+    return 0;
+  }
   return defaultOpen.value ? SIDEBAR_WIDTH : SIDEBAR_WIDTH_ICON;
 });
 
