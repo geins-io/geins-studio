5df5f0f7563873ab6d07e359a3a51f9b6a601650
Olivia Axelsson <axelsson@geins.io>
2026-02-10 14:01:25 +0100

    refactor: nuxt 4 typescript + tailwind changes

--- app/components/ui/item/index.ts
+++ app/components/ui/item/index.ts
@@ -13,7 +13,7 @@ export { default as ItemSeparator } from './ItemSeparator.vue';
 export { default as ItemTitle } from './ItemTitle.vue';
 
 export const itemVariants = cva(
-  'group/item [a]:hover:bg-accent/50 focus-visible:border-ring focus-visible:ring-ring/50 flex flex-wrap items-center rounded-md border border-transparent text-sm transition-colors duration-100 outline-none focus-visible:ring-[3px] [a]:transition-colors',
+  'group/item [a]:hover:bg-accent/50 focus-visible:border-ring focus-visible:ring-ring/50 flex flex-wrap items-center rounded-md border border-transparent text-sm transition-colors duration-100 outline-hidden focus-visible:ring-[3px] [a]:transition-colors',
   {
     variants: {
       variant: {
