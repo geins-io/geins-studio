2fb606201bbd27ad9ab7023224962576250b9941
arvidsson-geins <arvidsson@geins.io>
2026-04-23 09:58:01 +0200

    chore: wip

--- app/components/ui/sidebar/SidebarTrigger.vue
+++ app/components/ui/sidebar/SidebarTrigger.vue
@@ -12,15 +12,9 @@ const { toggleSidebar } = useSidebar();
 </script>
 
 <template>
-  <Button
-    data-sidebar="trigger"
-    data-slot="sidebar-trigger"
-    variant="ghost"
-    size="icon"
-    :class="cn('h-7 w-7', props.class)"
-    @click="toggleSidebar"
-  >
+  <Button data-sidebar="trigger" data-slot="sidebar-trigger" variant="ghost" size="icon"
+    :class="cn('h-7 w-7', props.class)" @click="toggleSidebar">
     <LucidePanelLeft />
-    <span class="sr-only">Toggle Sidebar</span>
+    <span class="sr-only">sToggle Sidebar</span>
   </Button>
 </template>
